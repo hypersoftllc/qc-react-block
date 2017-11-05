c07cf5f5c2e37cdec880de9731a3ad486cd23362
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2017-11-05 14:42:50 -0700

    Add Block Component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import styles from './index.css'
+
+/**
+ * @dependency Block.css
+ * @dependency ES6
+ * @dependency React
+ */
+
+
+export default function Block(props) {
+  let attrs, className
+
+  attrs = Object.assign({}, props)
+  className = []
+
+  if (props.compClassName) {
+    className.push(props.compClassName)
+  }
+  if (props.className) {
+    className.push(props.className)
+  }
+
+  attrs.className = className.join(' ')
+
+  delete attrs.children
+  delete attrs.compClassName
+
+  return (
+    <div {...attrs}>
+      {props.children}
+    </div>
+  )
+}
+    
+Block.defaultProps = {
+  compClassName: 'Block',
+}
