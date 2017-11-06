7dcc6311a235c0749ecdbee232c8657c214d90df
Danny Hurlburt <dhurlburtusa@users.noreply.github.com>
2017-11-05 21:23:52 -0700

    Finish Removing Hosting Instructions

--- scripts/build.js
+++ scripts/build.js
@@ -22,7 +22,6 @@ const config = require('../config/webpack.config.prod');
 const paths = require('../config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
 const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
 const printBuildError = require('react-dev-utils/printBuildError');
 
@@ -77,18 +76,6 @@ measureFileSizesBeforeBuild(paths.appBuild)
         WARN_AFTER_CHUNK_GZIP_SIZE
       );
       console.log();
-
-      // const appPackage = require(paths.appPackageJson);
-      // const publicUrl = paths.publicUrl;
-      // const publicPath = config.output.publicPath;
-      // const buildFolder = path.relative(process.cwd(), paths.appBuild);
-      // printHostingInstructions(
-      //   appPackage,
-      //   publicUrl,
-      //   publicPath,
-      //   buildFolder,
-      //   false
-      // );
     },
     err => {
       console.log(chalk.red('Failed to compile.\n'));
